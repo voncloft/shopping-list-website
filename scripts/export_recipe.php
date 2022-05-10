e376c0bdf50a3dd6e1af58ef1a3e5d039a3abce1
Voncloft <voncloft@gmail.com>
2022-05-10 07:02:51 -0400

    Minor Updates

--- scripts/export_recipe.php
+++ scripts/export_recipe.php
@@ -1,8 +1,9 @@
 <?php
     include '../include/passwords.php';
     $recipe_name=$_POST["recipe_name"]."_recipe_table";
+    $final_table_name=str_replace(" ","_",$recipe_name);
     echo $recipe_name."<br><br>";
-         $table_string="create table ".$recipe_name." (id INT AUTO_INCREMENT PRIMARY KEY, qty INT NOT NULL, ingredient_name text);";
+         $table_string="create table ".$final_table_name." (id INT AUTO_INCREMENT PRIMARY KEY, qty INT NOT NULL, ingredient_name text);";
          echo $table_string."<br>";
      echo "<br>";
     if ($conn->query($table_string)){
@@ -19,7 +20,7 @@
         $key="ingredient".$c;       
         if (!empty($_POST[$qty]))
         {
-            $insert_command="insert into ".$recipe_name."(qty,ingredient_name)VALUES('".$_POST[$qty]."','".$_POST[$key]."');";
+            $insert_command="insert into ".$final_table_name."(qty,ingredient_name)VALUES('".$_POST[$qty]."','".$_POST[$key]."');";
             echo $insert_command."<br>";
             if ($conn->query($insert_command)){
                 echo "Imported Ingredient";
