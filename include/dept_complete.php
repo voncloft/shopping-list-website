ae013e90b789f962d00dcace296358273fca8122
Voncloft <voncloft@gmail.com>
2022-05-13 22:11:05 -0400

    Minor Updates

---
+++ include/dept_complete.php
@@ -0,0 +1,20 @@
+ <?php
+//error_reporting(0);
+ $q=$_GET['q'];
+// echo $q;
+include_once '../include/passwords.php';
+ $my_data=mysqli_real_escape_string($conn,$q);
+ //echo $my_data;
+ //$sql="SELECT distinct Search_query FROM Search_History WHERE Search_query LIKE '%$my_data%' ORDER BY Search_query";
+ $sql="SELECT distinct department FROM items WHERE department LIKE '%$my_data%'";
+//echo $sql; 
+ $result = mysqli_query($conn,$sql) or die(mysqli_error());
+
+ if($result)
+ {
+  while($row=mysqli_fetch_array($result))
+  {
+   echo $row['department']."\n";
+  }
+ }
+?>
