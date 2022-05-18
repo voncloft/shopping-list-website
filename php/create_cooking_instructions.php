828fc4ac5425df2f78f3f733cacc536f019177f1
Voncloft <voncloft@gmail.com>
2022-05-18 06:36:48 -0400

    Minor Updates

---
+++ php/create_cooking_instructions.php
@@ -0,0 +1,7 @@
+Have for loop of 20 of the following
+
+Qty text box, Measurement pulled from measurement table, Drop down of items for recipe item
+
+Beneath the item selection of crap above have an instruction list textbox below that allows to combine
+
+Click submit - and create recipename.html in ../recipes folder
