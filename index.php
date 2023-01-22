4209bfed6c041b91a41192b5fcc2ef60d784f547
Voncloft <voncloft@gmail.com>
2023-01-22 07:13:51 -0500

    Minor Updates

--- index.php
+++ index.php
@@ -24,5 +24,5 @@ Shopping<br>
 5. Implement personal recipe editor - have it pull cals and macros from items<br>
 6. Have items use macros and calories<br>
 7. Final page and recipe lists will show calories and macros<br>
-
+8. Exercise Tracker (weight, treadmill, muscle, etc)
 
