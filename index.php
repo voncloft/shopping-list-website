dd2d774c1f9d073d18a410f1401bc8f654a2cad0
Voncloft <voncloft@gmail.com>
2023-01-22 07:02:56 -0500

    Minor Updates

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
 <b>
 Item Actions<br>
 <a href="../php/add_foods.php">Add Grocery Items to Database</a><br>
 <a href="../php/modify_foods.php">Modify Grocery Item</a><br>
+<a href="../php/pantry.php">Pantry Inventory</a><br>
 <br>
 Shopping List Actions<br>
 <a href="../php/create_shopping_lists.php">Create Shopping List</a><br>
@@ -13,7 +16,11 @@ Shopping List Actions<br>
 <br>
 Shopping<br>
 <a href="../php/final_shopping_list.php">View Final Shopping List for Week</a><br>
-
+<br><b><u>To Do:</u></b><br>
+1. Check box and have it marked in database<br>
+2. Edit Final table with ajax in pantry quantity and click ok <br>
+3. Ability to rename a recipe<br>
+4. Delete recipes and items<br>
 
 
 
