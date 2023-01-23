86facd6f5a1f3f87da34003ce406f37b7a3316ad
Voncloft <voncloft@gmail.com>
2023-01-22 23:06:38 -0500

    Minor Updates

--- ajax/checkbox.php
+++ ajax/checkbox.php
@@ -1,8 +1,5 @@
 <?php
 include '../include/passwords.php';
-//$test="";
-//$id=$_POST['id'];
-//if($_POST['IFRT_CHECKBOX']==1) {
 	if($_POST['checked']=="true")
 	{
 		$query="UPDATE final_list SET checked=1 WHERE IFRT=".$_POST['id'];
