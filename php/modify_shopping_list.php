58f9f6745cc328cbecfd9808439e411dac393103
Voncloft <voncloft@gmail.com>
2023-01-28 16:46:20 -0500

    Minor Updates

--- php/modify_shopping_list.php
+++ php/modify_shopping_list.php
@@ -1,34 +1,14 @@
 <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-
 <?php
-
-    include '../include/passwords.php';
+include_once '../include/passwords.php';
+include_once '../functions/show_recipes_interface.php';
 include '../include/toolbar.html';
 echo "<br>Recipes Imported Currently";
 echo "<br>";
-
-    ?>
-    <form action='../scripts/modify_shopping_list.php' method=POST>
-    <?php
-    $showtables= mysqli_query($conn, "SHOW TABLES FROM shopping_list");    
-    $sql="select recipes from current_week_recipes";
-    $result = $conn->query($sql);
-    $rows = $result->fetch_all(MYSQLI_ASSOC);
-     $i=0;      
-     while($table = mysqli_fetch_array($showtables)) { 
-      $i++;
-          foreach($rows as $recipe)
-          {
-            if($table[0]==$recipe['recipes'])
-            {            
-              $remove_table_name=str_replace("_recipe_table","",$table[0]);
-              $replaceunderscores=str_replace("_"," ",$remove_table_name);
-              echo '<input type="checkbox" name="recipes[]" value="'.$table[0]. '">';  
-              echo '<label">'.$replaceunderscores.'</label><br>';
-          }
-      }
-    }
- 
-     ?>
-     <input type="submit">
-     </form>
+?>
+<form action='../scripts/modify_shopping_list.php' method=POST>
+<?php
+	get_interface("show_imported","","");
+?>
+<input type="submit">
+</form>
