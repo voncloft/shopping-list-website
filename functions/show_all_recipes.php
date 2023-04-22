e048702dfcc48c27aab06b24fe85a31d31becf8f
Voncloft <voncloft@gmail.com>
2023-04-22 01:26:22 -0400

    Minor Updates

--- functions/show_all_recipes.php
+++ functions/show_all_recipes.php
@@ -1,8 +1,10 @@
 <?php
 include_once '../include/passwords.php';
+include_once 'get_prices_and_macros.php';
 function show_all_recipes_to_select($function_of_use)
 {
 	 global $conn;
+	 echo "<br>";
     $showtables= mysqli_query($conn, "SHOW TABLES FROM shopping_list");
     $sql="select recipes from current_week_recipes";
     $result = $conn->query($sql);
@@ -18,6 +20,7 @@ function show_all_recipes_to_select($function_of_use)
               if($table[0]!="final_list" && $table[0]!="current_week_recipes" && $table[0]!="items" && $table[0]!="recipe_lists"  && $table[0]!="measurements" && $table[0]!="weekly_total" && $table[0]!="variables" )
               {
                   $i++;
+
                   $remove_rt=str_replace("_recipe_table","",$table[0]);
                   $remove_us=str_replace("_"," ",$remove_rt);
                   switch($function_of_use)
@@ -32,6 +35,8 @@ function show_all_recipes_to_select($function_of_use)
                   		echo '<input type="checkbox" name="recipes[]" value="'.$table[0]. '">';  
                   		echo '<label">'.$remove_us.'</label><br>';                				
              	}
+             	                  get_prices_and_macros($table[0]);
+             	                  echo "<br>";
               }
             }
          }
