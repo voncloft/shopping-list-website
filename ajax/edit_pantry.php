a60876bd02c91e7244db9ea57ee66558a93c2194
Voncloft <voncloft@gmail.com>
2023-10-26 11:34:23 -0400

    Minor Updates

--- ajax/edit_pantry.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../include/passwords.php';
-include '../functions/saveFinalPriceToDB.php';
-$query="UPDATE items SET pantryqty =".$_POST['updated_value']." WHERE id=".$_POST['id'];
-mysqli_query($conn,$query);
-saveFinalPriceToDB($_POST['price']);
-?>
--- ajax/portions.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include '../include/passwords.php';
-	$query="update variables set servings =".$_POST['id']." where servings >0";
-	mysqli_query($conn,$query);
-?>
