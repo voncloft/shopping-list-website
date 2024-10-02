e21179b5f8f7582ea68481f24633d312dfef8b1a
Voncloft <voncloft@gmail.com>
2024-10-02 07:33:36 -0400

    Minor Updates

---
+++ php/test.php
@@ -0,0 +1,9 @@
+ <html>
+<body>
+<?php
+//$html = file_get_contents("https://www.walmart.com/ip/Great-Value-Cherry-Berry-Blend-Frozen-48-oz-in-resealable-bag");
+//echo $html
+var_dump(stream_get_wrappers());
+?>
+</body>
+</html> 
